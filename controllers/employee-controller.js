6e039f0bc727dbd70b17ee40d14d6b09937eb698
Erfan Ghazimoradi <erfan.ghazimoradi.work@gmail.com>
2023-04-23 16:15:21 +0330

    add api query features - hw17

--- controllers/employee-controller.js
+++ controllers/employee-controller.js
@@ -3,7 +3,21 @@ const Employee = require('../models/employee-model');
 
 const getAllEmployees = async (req, res, next) => {
 	try {
-		const employees = await Employee.find({});
+		const {
+			page = 1,
+			limit = 10,
+			sort: sortBy = '-createdAt',
+			fields = '-__v',
+			...filter
+		} = req.query;
+
+		const skip = (page * 1 - 1) * limit * 1;
+
+		const employees = await Employee.find(filter)
+			.select(fields.split(','))
+			.sort(sortBy)
+			.skip(skip)
+			.limit(Number(limit));
 
 		res.json({
 			status: 'success',
@@ -17,6 +31,8 @@ const getAllEmployees = async (req, res, next) => {
 const getEmployeeById = async (req, res, next) => {
 	try {
 		const { id: employeeId } = req.params;
+
+		// check company first
 
 		const employee = await Employee.findById(employeeId).populate('company');
 
